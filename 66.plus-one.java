91a3d0241eacf9d41be9195cedc2fff3ef815493
chaoyi.liu <chaoyi.liu@maryland.gov>
2019-12-13 17:42:35 -0500

    Dec 13 2019 17:42

---
+++ 66.plus-one.java
@@ -0,0 +1,62 @@
+/*
+ * @lc app=leetcode id=66 lang=java
+ *
+ * [66] Plus One
+ *
+ * https://leetcode.com/problems/plus-one/description/
+ *
+ * algorithms
+ * Easy (41.80%)
+ * Likes:    1101
+ * Dislikes: 1889
+ * Total Accepted:    480.4K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '[1,2,3]'
+ *
+ * Given a non-empty array of digits representing a non-negative integer, plus
+ * one to the integer.
+ * 
+ * The digits are stored such that the most significant digit is at the head of
+ * the list, and each element in the array contain a single digit.
+ * 
+ * You may assume the integer does not contain any leading zero, except the
+ * number 0 itself.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [1,2,3]
+ * Output: [1,2,4]
+ * Explanation: The array represents the integer 123.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [4,3,2,1]
+ * Output: [4,3,2,2]
+ * Explanation: The array represents the integer 4321.
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int n = digits.length - 1;
+        for (int i = n; i >= 0; i--) {
+            if (digits[i] == 9) {
+                digits[i] = 0;
+            } else {
+                digits[i]++;
+                break;
+            }
+        }
+        if (digits[0] == 0) {
+            int[] ret = new int[n + 2];
+            ret[0] = 1;
+            return ret;
+        }
+        return digits;
+    }
+}
+// @lc code=end
