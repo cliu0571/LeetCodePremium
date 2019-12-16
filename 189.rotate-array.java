1a96a5908a3a2c38785cbb26e456811f6c008838
chaoyi.liu <chaoyi.liu@maryland.gov>
2019-12-16 13:46:02 -0500

    Dec 16 2019 13:45

---
+++ 189.rotate-array.java
@@ -0,0 +1,75 @@
+/*
+ * @lc app=leetcode id=189 lang=java
+ *
+ * [189] Rotate Array
+ *
+ * https://leetcode.com/problems/rotate-array/description/
+ *
+ * algorithms
+ * Easy (32.26%)
+ * Likes:    1883
+ * Dislikes: 697
+ * Total Accepted:    379.4K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '[1,2,3,4,5,6,7]\n3'
+ *
+ * Given an array, rotate the array to the right by k steps, where k is
+ * non-negative.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [1,2,3,4,5,6,7] and k = 3
+ * Output: [5,6,7,1,2,3,4]
+ * Explanation:
+ * rotate 1 steps to the right: [7,1,2,3,4,5,6]
+ * rotate 2 steps to the right: [6,7,1,2,3,4,5]
+ * rotate 3 steps to the right: [5,6,7,1,2,3,4]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [-1,-100,3,99] and k = 2
+ * Output: [3,99,-1,-100]
+ * Explanation: 
+ * rotate 1 steps to the right: [99,-1,-100,3]
+ * rotate 2 steps to the right: [3,99,-1,-100]
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * Try to come up as many solutions as you can, there are at least 3 different
+ * ways to solve this problem.
+ * Could you do it in-place with O(1) extra space?
+ * 
+ */
+
+// @lc code=start
+/**
+ * using reverse Original List : 1 2 3 4 5 6 7 After reversing all numbers : 7 6
+ * 5 4 3 2 1 After reversing first k numbers : 5 6 7 4 3 2 1 After revering last
+ * n-k numbers : 5 6 7 1 2 3 4 --> Result
+ */
+class Solution {
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    private void reverse(int[] nums, int left, int right){
+        while(left < right){
+            swap(nums, left++, right--);
+        }
+    }
+
+    private void swap(int[] nums, int left, int right) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+    }
+}
+// @lc code=end
