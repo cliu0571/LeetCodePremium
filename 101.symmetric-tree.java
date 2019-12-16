6dd9d745476e5c0e3df9fa200f6f0ee0fe3169c6
chaoyi.liu <chaoyi.liu@maryland.gov>
2019-12-16 15:54:03 -0500

    Dec 16 2019 15:53

---
+++ 101.symmetric-tree.java
@@ -0,0 +1,67 @@
+/*
+ * @lc app=leetcode id=101 lang=java
+ *
+ * [101] Symmetric Tree
+ *
+ * https://leetcode.com/problems/symmetric-tree/description/
+ *
+ * algorithms
+ * Easy (45.09%)
+ * Likes:    2954
+ * Dislikes: 69
+ * Total Accepted:    510.3K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '[1,2,2,3,4,4,3]'
+ *
+ * Given a binary tree, check whether it is a mirror of itself (ie, symmetric
+ * around its center).
+ * 
+ * For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+ * 
+ * 
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   2
+ * ⁠/ \ / \
+ * 3  4 4  3
+ * 
+ * 
+ * 
+ * 
+ * But the following [1,2,2,null,3,null,3] is not:
+ * 
+ * 
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   2
+ * ⁠  \   \
+ * ⁠  3    3
+ * 
+ * 
+ * 
+ * 
+ * Note:
+ * Bonus points if you could solve it both recursively and iteratively.
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode(int x) { val = x; } }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return root == null || isSymmetric(root.left, root.right);
+    }
+
+    private boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null || right == null)
+            return left == right;
+        if (left.val != right.val) {
+            return false;
+        }
+        return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+    }
+}
+// @lc code=end
---
+++ 119.pascals-triangle-ii.java
@@ -0,0 +1,54 @@
+import java.util.Arrays;
+
+/*
+ * @lc app=leetcode id=119 lang=java
+ *
+ * [119] Pascal's Triangle II
+ *
+ * https://leetcode.com/problems/pascals-triangle-ii/description/
+ *
+ * algorithms
+ * Easy (46.12%)
+ * Likes:    590
+ * Dislikes: 182
+ * Total Accepted:    239.2K
+ * Total Submissions: 518.2K
+ * Testcase Example:  '3'
+ *
+ * Given a non-negative index k where k ≤ 33, return the k^th index row of the
+ * Pascal's triangle.
+ * 
+ * Note that the row index starts from 0.
+ * 
+ * 
+ * In Pascal's triangle, each number is the sum of the two numbers directly
+ * above it.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: 3
+ * Output: [1,3,3,1]
+ * 
+ * 
+ * Follow up:
+ * 
+ * Could you optimize your algorithm to use only O(k) extra space?
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        Integer[] array = new Integer[rowIndex + 1];
+        Arrays.fill(array, 0);
+        array[0] = 1;
+        for (int i = 1; i < array.length; i++) {
+            for (int j = i; j > 0; j--) {
+                array[j] = array[j] + array[j - 1];
+            }
+        }
+        return Arrays.asList(array);
+    }
+}
+// @lc code=end
