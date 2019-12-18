b846cbd0d059dff228bc2fc510cf2a520c66867d
chaoyi.liu <chaoyi.liu@maryland.gov>
2019-12-18 15:52:39 -0500

    Dec 18 2019 15:52 [287,33,81] Binary search

--- 287.find-the-duplicate-number.java
+++ 287.find-the-duplicate-number.java
@@ -45,19 +45,20 @@
 // @lc code=start
 class Solution {
     public int findDuplicate(int[] nums) {
+        // fast catch slow means there is a loop
         int slow = nums[0], fast = nums[0];
         do {
             slow = nums[slow];
             fast = nums[nums[fast]];
         } while (slow != fast);
 
-        // find the start of loop
+        // find the start of loop -> duplicate num
         int ans = nums[0];
         while (ans != slow) {
             ans = nums[ans];
             slow = nums[slow];
         }
-        return ans;
+        return slow;
     }
 }
 // @lc code=end
---
+++ 33.search-in-rotated-sorted-array.java
@@ -0,0 +1,69 @@
+/*
+ * @lc app=leetcode id=33 lang=java
+ *
+ * [33] Search in Rotated Sorted Array
+ *
+ * https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+ *
+ * algorithms
+ * Medium (33.43%)
+ * Likes:    3371
+ * Dislikes: 382
+ * Total Accepted:    532K
+ * Total Submissions: 1.6M
+ * Testcase Example:  '[4,5,6,7,0,1,2]\n0'
+ *
+ * Suppose an array sorted in ascending order is rotated at some pivot unknown
+ * to you beforehand.
+ * 
+ * (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+ * 
+ * You are given a target value to search. If found in the array return its
+ * index, otherwise return -1.
+ * 
+ * You may assume no duplicate exists in the array.
+ * 
+ * Your algorithm's runtime complexity must be in the order of O(log n).
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: nums = [4,5,6,7,0,1,2], target = 0
+ * Output: 4
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: nums = [4,5,6,7,0,1,2], target = 3
+ * Output: -1
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int mid = left;
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (nums[mid] == target)
+                return mid;
+            if (nums[left] <= nums[mid]) {// left - mid is sorted
+                if (nums[left] <= target && target <= nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {// left - mid is not sorted, mid - right is sorted
+                if (nums[mid] <= target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
+// @lc code=end
