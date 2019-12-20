88ab038c0d7f11290bf7e896b30e63aa02a27f27
chaoyi.liu <chaoyi.liu@maryland.gov>
2019-12-20 15:18:34 -0500

    Dec 20 2019 15:18 [203,206]

---
+++ 203.remove-linked-list-elements.java
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=203 lang=java
+ *
+ * [203] Remove Linked List Elements
+ *
+ * https://leetcode.com/problems/remove-linked-list-elements/description/
+ *
+ * algorithms
+ * Easy (36.77%)
+ * Likes:    1090
+ * Dislikes: 61
+ * Total Accepted:    268.6K
+ * Total Submissions: 729.9K
+ * Testcase Example:  '[1,2,6,3,4,5,6]\n6'
+ *
+ * Remove all elements from a linked list of integers that have value val.
+ * 
+ * Example:
+ * 
+ * 
+ * Input:  1->2->6->3->4->5->6, val = 6
+ * Output: 1->2->3->4->5
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list. public class ListNode { int val; ListNode
+ * next; ListNode(int x) { val = x; } }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        if (head == null)
+            return head;
+        head.next = removeElements(head.next, val);
+        return head.val == val ? head.next : head;
+    }
+}
+// @lc code=end
---
+++ 206.reverse-linked-list.java
@@ -0,0 +1,71 @@
+/*
+ * @lc app=leetcode id=206 lang=java
+ *
+ * [206] Reverse Linked List
+ *
+ * https://leetcode.com/problems/reverse-linked-list/description/
+ *
+ * algorithms
+ * Easy (58.30%)
+ * Likes:    3194
+ * Dislikes: 77
+ * Total Accepted:    763.8K
+ * Total Submissions: 1.3M
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * Reverse a singly linked list.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: 1->2->3->4->5->NULL
+ * Output: 5->4->3->2->1->NULL
+ * 
+ * 
+ * Follow up:
+ * 
+ * A linked list can be reversed either iteratively or recursively. Could you
+ * implement both?
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list. public class ListNode { int val; ListNode
+ * next; ListNode(int x) { val = x; } }
+ */
+class Solution {
+    /** Iterative solution */
+    public ListNode reverseList(ListNode head) {
+        /** Iterative solution */
+        // return reverseListIterative(head);
+        /** Recursive solution */
+        return reverseListRecursive(head);
+    }
+
+    /** Iterative solution */
+    private ListNode reverseListIterative(ListNode head) {
+        ListNode ans = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = ans;
+            ans = head;
+            head = next;
+        }
+        return ans;
+    }
+
+    /* recursive solution */
+    private ListNode reverseListRecursive(ListNode head) {
+        return reverseListInt(head, null);
+    }
+
+    private ListNode reverseListInt(ListNode head, ListNode newHead) {
+        if (head == null)
+            return newHead;
+        ListNode next = head.next;
+        head.next = newHead;
+        return reverseListInt(next, head);
+    }
+}
+// @lc code=end
