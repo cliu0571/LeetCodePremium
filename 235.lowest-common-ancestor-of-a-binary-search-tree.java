36b9cf71328030f8db3477256bef9a6f8e69ae63
chaoyi.liu <chaoyi.liu@maryland.gov>
2020-02-28 14:39:23 -0500

    Feb 28 2020 14:39 LCA

---
+++ 235.lowest-common-ancestor-of-a-binary-search-tree.java
@@ -0,0 +1,84 @@
+/*
+ * @lc app=leetcode id=235 lang=java
+ *
+ * [235] Lowest Common Ancestor of a Binary Search Tree
+ *
+ * https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/description/
+ *
+ * algorithms
+ * Easy (47.23%)
+ * Likes:    1627
+ * Dislikes: 94
+ * Total Accepted:    358.9K
+ * Total Submissions: 747.1K
+ * Testcase Example:  '[6,2,8,0,4,7,9,null,null,3,5]\n2\n8'
+ *
+ * Given a binary search tree (BST), find the lowest common ancestor (LCA) of
+ * two given nodes in the BST.
+ * 
+ * According to the definition of LCA on Wikipedia: “The lowest common ancestor
+ * is defined between two nodes p and q as the lowest node in T that has both p
+ * and q as descendants (where we allow a node to be a descendant of itself).”
+ * 
+ * Given binary search tree:  root = [6,2,8,0,4,7,9,null,null,3,5]
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+ * Output: 6
+ * Explanation: The LCA of nodes 2 and 8 is 6.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+ * Output: 2
+ * Explanation: The LCA of nodes 2 and 4 is 2, since a node can be a descendant
+ * of itself according to the LCA definition.
+ * 
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * All of the nodes' values will be unique.
+ * p and q are different and both values will exist in the BST.
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (root == p || root == q){
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left != null && right != null){
+            return root;
+        }
+        return left == null ? right : left;
+    }
+}
+// @lc code=end
+
