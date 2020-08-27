85e96cdc10923838204b3fc3f7d53d321ef19074
chaoyi.liu <chaoyi.liu@maryland.gov>
2020-08-27 03:49:15 -0400

    Tree 08-26-2020

---
+++ 102.binary-tree-level-order-traversal.java
@@ -0,0 +1,78 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/*
+ * @lc app=leetcode id=102 lang=java
+ *
+ * [102] Binary Tree Level Order Traversal
+ *
+ * https://leetcode.com/problems/binary-tree-level-order-traversal/description/
+ *
+ * algorithms
+ * Medium (51.19%)
+ * Likes:    2308
+ * Dislikes: 62
+ * Total Accepted:    519.5K
+ * Total Submissions: 995.1K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * Given a binary tree, return the level order traversal of its nodes' values.
+ * (ie, from left to right, level by level).
+ * 
+ * 
+ * For example:
+ * Given binary tree [3,9,20,null,null,15,7],
+ * 
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ * 
+ * 
+ * 
+ * return its level order traversal as:
+ * 
+ * [
+ * ⁠ [3],
+ * ⁠ [9,20],
+ * ⁠ [15,7]
+ * ]
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        List<List<Integer>> wrapList = new LinkedList<List<Integer>>();
+        
+        if(root == null) return wrapList;
+        
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            int levelNum = queue.size();
+            List<Integer> subList = new LinkedList<Integer>();
+            for(int i=0; i<levelNum; i++) {
+                if(queue.peek().left != null) queue.offer(queue.peek().left);
+                if(queue.peek().right != null) queue.offer(queue.peek().right);
+                subList.add(queue.poll().val);
+            }
+            wrapList.add(subList);
+        }
+        return wrapList;
+    }
+}
+// @lc code=end
+
---
+++ 872.leaf-similar-trees.java
@@ -0,0 +1,62 @@
+/*
+ * @lc app=leetcode id=872 lang=java
+ *
+ * [872] Leaf-Similar Trees
+ *
+ * https://leetcode.com/problems/leaf-similar-trees/description/
+ *
+ * algorithms
+ * Easy (64.59%)
+ * Likes:    827
+ * Dislikes: 42
+ * Total Accepted:    95.7K
+ * Total Submissions: 148.2K
+ * Testcase Example:  '[3,5,1,6,2,9,8,null,null,7,4]\n[3,5,1,6,7,4,2,null,null,null,null,null,null,9,8]'
+ *
+ * Consider all the leaves of a binary tree.  From left to right order, the
+ * values of those leaves form a leaf value sequence.
+ * 
+ * 
+ * 
+ * For example, in the given tree above, the leaf value sequence is (6, 7, 4,
+ * 9, 8).
+ * 
+ * Two binary trees are considered leaf-similar if their leaf value sequence is
+ * the same.
+ * 
+ * Return true if and only if the two given trees with head nodes root1 and
+ * root2 are leaf-similar.
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * Both of the given trees will have between 1 and 200 nodes.
+ * Both of the given trees will have values between 0 and 200
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        
+    }
+}
+// @lc code=end
+
