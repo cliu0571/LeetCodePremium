15678da2c4ab5ddf40e2bd881458769b37f1ca2c
cliu0571 <cliu0571@gmail.com>
2021-10-15 23:40:15 -0400

    reverse node in k group

--- 25.reverse-nodes-in-k-group.java
+++ 25.reverse-nodes-in-k-group.java
@@ -6,19 +6,73 @@
 
 // @lc code=start
 /**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
+ * Definition for singly-linked list. public class ListNode { int val; ListNode
+ * next; ListNode() {} ListNode(int val) { this.val = val; } ListNode(int val,
+ * ListNode next) { this.val = val; this.next = next; } }
  */
 class Solution {
+    
     public ListNode reverseKGroup(ListNode head, int k) {
-        
+
+        int count = 0;
+        ListNode curr = head;
+
+        // First, see if there are atleast k nodes
+        // left in the linked list.
+        while (count < k && curr != null) {
+            curr = curr.next;
+            count++;
+        }
+
+        // If we have k nodes, then we reverse them
+        if (count == k) {
+
+            // Reverse the first k nodes of the list and
+            // get the reversed list's head.
+            ListNode reversedHead = this.reverseLinkedList(head, k);
+
+            // Now recurse on the remaining linked list. Since
+            // our recursion returns the head of the overall processed
+            // list, we use that and the "original" head of the "k" nodes
+            // to re-wire the connections.
+            head.next = this.reverseKGroup(curr, k);
+            return reversedHead;
+        }
+
+        return head;
     }
+
+    public ListNode reverseLinkedList(ListNode head, int k) {
+        
+        // Reverse k nodes of the given linked list.
+        // This function assumes that the list contains 
+        // atleast k nodes.
+        ListNode new_head = null;
+        ListNode ptr = head;
+        
+        while (k > 0) {
+            
+            // Keep track of the next node to process in the
+            // original list
+            ListNode next_node = ptr.next;
+            
+            // Insert the node pointed to by "ptr"
+            // at the beginning of the reversed list
+            ptr.next = new_head;
+            new_head = ptr;
+            
+            // Move on to the next node
+            ptr = next_node;
+            
+            // Decrement the count of nodes to be reversed by 1
+            k--;
+        }
+            
+            
+        // Return the head of the reversed list
+        return new_head;
+    
+    }
+
 }
 // @lc code=end
-
