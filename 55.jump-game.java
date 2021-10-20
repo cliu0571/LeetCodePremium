34b367e5c885aac4135d6e295e4ad2a33629bfe5
cliu0571 <cliu0571@gmail.com>
2021-10-19 23:21:35 -0400

    jump game

---
+++ 55.jump-game.java
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=55 lang=java
+ *
+ * [55] Jump Game
+ */
+
+// @lc code=start
+class Solution {
+    public boolean canJump(int[] nums) {
+        // greedy T: O(n) S: O(1)
+        int lastPos = nums.length - 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (i + nums[i] >= lastPos) {
+                lastPos = i;
+            }
+        }
+        return lastPos == 0;
+    }
+}
+// @lc code=end
