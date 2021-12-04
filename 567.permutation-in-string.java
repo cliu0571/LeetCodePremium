75d83f2f50b94a46fe12a74daa445eda9867e668
cliu0571 <cliu0571@gmail.com>
2021-12-03 19:32:19 -0500

    Create 567.permutation-in-string.java

---
+++ 567.permutation-in-string.java
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=567 lang=java
+ *
+ * [567] Permutation in String
+ */
+
+// @lc code=start
+class Solution {
+    // sliding window
+    // T O(l1 + 26*(l2-l1)) S O(1)
+    public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length())
+            return false;
+        int[] s1map = new int[26];
+        int[] s2map = new int[26];
+
+        for (int i = 0; i < s1.length(); i++) {
+            s1map[s1.charAt(i) - 'a']++;
+            s2map[s2.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < s2.length() - s1.length(); i++) {
+            if (matches(s1map, s2map))
+                return true;
+            s2map[s2.charAt(i) - 'a']--;
+            s2map[s2.charAt(i + s1.length()) - 'a']++;
+        }
+
+        return matches(s1map, s2map);
+    }
+
+    public boolean matches(int[] s1map, int[] s2map) {
+        for (int i = 0; i < 26; i++) {
+            if (s1map[i] != s2map[i])
+                return false;
+        }
+        return true;
+    }
+}
+// @lc code=end
