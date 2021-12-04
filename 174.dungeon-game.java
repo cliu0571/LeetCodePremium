edc6c66cb97e725df4948838d1a251f831c342b3
cliu0571 <cliu0571@gmail.com>
2021-12-04 02:29:09 -0500

    Update 174.dungeon-game.java

--- 174.dungeon-game.java
+++ 174.dungeon-game.java
@@ -9,7 +9,6 @@ import java.util.Arrays;
 // @lc code=start
 class Solution {
     // T O(M*N), S O(M*N)
-    int inf = Integer.MAX_VALUE;
     int[][] dp;
     int rows, cols;
 
@@ -17,34 +16,34 @@ class Solution {
         this.rows = dungeon.length;
         this.cols = dungeon[0].length;
         this.dp = new int[rows][cols];
-        
+
         for (int[] arr : this.dp) {
-            Arrays.fill(arr, this.inf);
+            Arrays.fill(arr, Integer.MAX_VALUE);
         }
 
         for (int row = this.rows - 1; row >= 0; --row) {
             for (int col = this.cols - 1; col >= 0; --col) {
                 int currCell = dungeon[row][col];
 
-                int rightHealth = getMinHealth(currCell, row, col + 1);
-                int downHealth = getMinHealth(currCell, row + 1, col);
-                int nextHealth = Math.min(rightHealth, downHealth);
+                int goRight = getMinHealth(currCell, row, col + 1);
+                int goDown = getMinHealth(currCell, row + 1, col);
+                int nextHp = Math.min(goRight, goDown);
 
-                int minHealth = inf;
-                if (nextHealth != inf) {
-                    minHealth = nextHealth;
+                int minHp = 1;
+                if (nextHp != Integer.MAX_VALUE) {
+                    minHp = nextHp;
                 } else {
-                    minHealth = currCell >= 0 ? 1 : 1 - currCell;
+                    minHp = currCell >= 0 ? 1 : 1 - currCell;
                 }
-                this.dp[row][col] = minHealth;
+                dp[row][col] = minHp;
             }
         }
         return this.dp[0][0];
     }
-    
+
     public int getMinHealth(int currCell, int nextRow, int nextCol) {
         if (nextRow >= this.rows || nextCol >= this.cols)
-            return inf;
+            return Integer.MAX_VALUE;
         int nextCell = this.dp[nextRow][nextCol];
         // hero needs at least 1 point to survive
         return Math.max(1, nextCell - currCell);
