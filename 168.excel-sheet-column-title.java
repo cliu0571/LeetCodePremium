3aed0b8f7f4b793750e3e01bb5de052e5815d66a
cliu0571 <cliu0571@gmail.com>
2021-12-04 01:10:13 -0500

    Update 168.excel-sheet-column-title.java

--- 168.excel-sheet-column-title.java
+++ 168.excel-sheet-column-title.java
@@ -6,6 +6,7 @@
 
 // @lc code=start
 class Solution {
+    // T O(n) S O(n)
     public String convertToTitle(int columnNumber) {
         return columnNumber == 0 ? "" : convertToTitle(--columnNumber / 26) + (char) ('A' + (columnNumber % 26));
     }
